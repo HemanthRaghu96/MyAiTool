0eb275bdb869273b43a0124ea1bc109fc9603ab6
HemanthRaghu96 <hemanthraghu96@gmail.com>
2025-10-22 18:47:46 +0530

    created dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: require.resolve("react"),
+      "react-dom": require.resolve("react-dom"),
+      "react/jsx-runtime": require.resolve("react/jsx-runtime"),
+    },
+  },
+  optimizeDeps: {
+    exclude: ["react", "react-dom"], // Prevent Vite from pre-bundling React
+  },
+});
